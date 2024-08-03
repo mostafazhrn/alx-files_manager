d12d6ecd95ce8a9349a79437cf5e18d913e96465
mostafazhrn <mostafazhrn@gmail.com>
2024-08-03 11:31:41 -0400

    push task0

---
+++ utils/redis.js
@@ -0,0 +1,28 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.get = promisify(this.client.get).bind(this.client);
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return this.get(key, (err, reply) => reply);
+    }
+
+    async set(key, value, duration) {
+        return this.client.set(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        return this.client.del(key);}
+}
+    
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
