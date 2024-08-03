891c9c0c2ee2d691f98147cf6e4c16a069e3f52d
mostafazhrn <mostafazhrn@gmail.com>
2024-08-03 08:37:52 -0700

    fix_task0

--- utils/redis.js
+++ utils/redis.js
@@ -2,27 +2,28 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.get = promisify(this.client.get).bind(this.client);
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.get = promisify(this.client.get).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return this.get(key, (err, reply) => reply);
-    }
+  async get(key) {
+    return this.get(key, (err, reply) => reply);
+  }
 
-    async set(key, value, duration) {
-        return this.client.set(key, value, 'EX', duration);
-    }
+  async set(key, value, duration) {
+    return this.client.set(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        return this.client.del(key);}
+  async del(key) {
+    return this.client.del(key);
+  }
 }
-    
+
 const redisClient = new RedisClient();
 
 module.exports = redisClient;
