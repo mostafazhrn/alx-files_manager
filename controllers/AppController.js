eba91414dbafa869dbb68f53eafea65ee1e17868
mostafazhrn <mostafazhrn@gmail.com>
2024-08-04 11:16:10 -0700

    push task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).send(status);
+  }
+
+  // continue the appcontroller code below
+
+  // continer the appcontroller code below
+  static async getStats(req, res) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).send(stats);
+  }
+}
+
+module.exports = AppController;
